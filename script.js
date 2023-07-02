f8aed7e5c90f057692332dbd12ace97a67603eea
Ivan Doroshenko <94081688+880023@users.noreply.github.com>
2023-07-02 13:55:41 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// const firstName = 'Doroshenko';
+// const lastName = 'Ivan';
+// // const age = 19;
+// const isProgrammer = true;
+
+// const _ = 'private';
+// const $ = 'some value';
+
+ // alert(`${_} ${$}`);
+
+// let сurrentYear = 2023;
+// const age = 19;
+// let birthsdayYear = сurrentYear - age;
+// alert(birthsdayYear);
+
+// const a = 2;
+// const b = 5;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(++сurrentYear);
+// console.log(сurrentYear < age);
+// console.log(сurrentYear >= age);
+// console.log(сurrentYear <= age);
+
